e0f790e3c4de574448a2f1356ff738bc20f463e9
sabrina bendraoua <sabbendra@gmail.com>
2023-04-16 16:45:35 +0200

    cloudinary

--- index.js
+++ index.js
@@ -12,9 +12,9 @@ mongoose.connect(process.env.MONGODB_URI);
 
 //connexion compte cloudinary
 cloudinary.config({
-  cloud_name: CLOUDINARY_CLOUD_NAME,
-  api_key: CLOUDINARY_API_KEY,
-  api_secret: CLOUDINARY_API_SECRET,
+  cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+  api_key: process.env.CLOUDINARY_API_KEY,
+  api_secret: process.env.CLOUDINARY_API_SECRET,
 });
 
 //J'importe mes fichiers user.js, offer.js et filter.js
